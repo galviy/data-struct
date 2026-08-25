75e7e9d12470be314ed16cd90ca3cace52da0950
Galvin <68190778+galviy@users.noreply.github.com>
2026-08-25 07:46:04 +0700

    Add files via upload

---
+++ pertemuan_1/maintitik.c
@@ -0,0 +1,20 @@
+/************************************/
+/* Program   : maintitik.c */
+/* Deskripsi : aplikasi driver modul Titik */
+/* NIM/Nama  : */
+/* Tanggal   : */
+/***********************************/
+#include <stdio.h>
+#include "titik.h"
+
+int main() {
+	//kamus main
+	Titik T1;
+	
+	//algoritma
+	//printf("Halo, ini driver modul Titik /n");
+	//makeTitik(&T1,4,5);
+	//printf("/nNilai absis = %d",getAbsis(T1));
+	
+	return 0;
+}
---
+++ pertemuan_1/titik.c
@@ -0,0 +1,9 @@
+/************************************/
+/* Program   : titik.c */
+/* Deskripsi : realisasi body modul Titik */
+/* NIM/Nama  : */
+/* Tanggal   : */
+/***********************************/
+#include <stdio.h>
+#include "titik.h"
+
