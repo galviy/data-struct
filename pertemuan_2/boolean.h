35215c9e596ce1d29bdbc5128e725e9f358c4c94
galviyy <galvindoser@gmail.com>
2026-09-02 08:28:59 +0700

    added tabel

---
+++ pertemuan_2/boolean.h
@@ -0,0 +1,8 @@
+#ifndef boolean_H
+#define boolean_H 
+
+
+typedef enum {false, true} boolean;
+
+
+#endif
---
+++ pertemuan_2/mainTabel.c
@@ -0,0 +1,13 @@
+/************************************/
+/* Program   : mainTabel.c */
+/* Deskripsi :  */
+/* NIM/Nama  :
+/* Tanggal   : */
+/***********************************/
+
+#include <stdio.h>
+#include "tabel.h"
+
+int main(){
+    Tabel T1;
+}
